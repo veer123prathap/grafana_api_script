d91e8bcd228847838eb091d9400ac2f2356cbc7c
veer123prathap <62214817+veer123prathap@users.noreply.github.com>
2024-01-22 20:15:17 -0600

    Update deleting_dit.sh

--- deleting_dit.sh
+++ deleting_dit.sh
@@ -7,14 +7,21 @@ excluded_items=(
     "keep_this_dir"    
 )
 
-for item in "${excluded_items[@]}"; do
-    find "$directory" -mindepth 1 -type d ! -path "$directory/$item/*" -print0 | while IFS= read -r -d '' dir; do
-    
-    echo "Directory to be deleted: $dir" 
-    read -p "Press enter to delete this directory or type anything else to skip: " -r
-    echo    # move to a new line
-    if [[ -z $REPLY ]]
-    then
-        rm -r "$dir"
+find "$directory" -mindepth 1 -type d -print0 | while IFS= read -r -d '' dir; do
+    exclude_flag=0
+    for item in "${excluded_items[@]}"; do
+        if [[ $dir == "$directory/$item" || $dir == "$directory/$item"* ]]; then
+            exclude_flag=1
+            break
+        fi
+    done
+    if [[ $exclude_flag -eq 0 ]]; then
+        echo "Directory to be deleted: $dir" 
+        read -p "Press enter to delete this directory or type anything else to skip: " -r
+        echo    # move to a new line
+        if [[ -z $REPLY ]]
+        then
+            rm -r "$dir"
+        fi
     fi
 done
